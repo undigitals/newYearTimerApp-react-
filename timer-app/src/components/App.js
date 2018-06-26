190474ec3384ea896b43a342aee4a579ef284199
Umid Negmatullayev <undigitals@gmail.com>
2018-06-26 18:04:20 +0900

    did the basic setup for app, install the bulma lib

---
+++ timer-app/src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Countdown from './Countdown';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>welcome to react</h1>
+
+        <Countdown/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ timer-app/src/components/Countdown.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Countdown = (props) =>
+<div>
+    <h1>New Year is coming up!!! </h1>
+</div>
+
+export default Countdown
